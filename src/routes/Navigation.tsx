24c8454e10a283e03a62136892541ef834dd0e75
cplan85 <carlos_planchart@live.com>
2023-02-24 23:09:37 +0100

    Basic lazyload complete

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,36 +1,43 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { Suspense } from 'react';
+
+// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+import { routes } from './routes';
 import logo from '../logo.svg'
 
 export const Navigation = () => {
+
+    const navlinks = routes.map((route) => 
+    <li key={route.to}>
+       <NavLink to={route.to} className={({isActive}) => isActive? 'nav-active' : '' }>{route.name}</NavLink> 
+    </li>);
+
+    const definedRoutes = routes.map(({path, Component}) => {
+    return <Route key={path} path={path} element={<Component/>} />
+})
+
+
   return (
-    <BrowserRouter>
+    <Suspense fallback={<span>Loading...</span>}>
+
+<BrowserRouter>
     <div className="main-layout">
     <nav>
         <img src={logo} alt="React Logo" />
-        <ul>
-        <li>
-    <NavLink to="/lazy1" className={({isActive}) => isActive? 'nav-active' : '' }>Lazy 1</NavLink>
-        </li>
-        <li>
-    <NavLink to="/lazy2" className={({isActive}) => isActive? 'nav-active' : '' }>Lazy 2</NavLink>
-        </li>
-        <li>
-    <NavLink to="/lazy3" className={({isActive}) => isActive? 'nav-active' : '' }>Lazy 3</NavLink>
-        </li>
+        <ul /* TODO: creatte dynamic */ >
+       {navlinks}
     </ul>
     </nav>
 
     <Routes>
-        <Route path="lazy1" element={<LazyPage1/>} />
-        <Route path="lazy2" element={<LazyPage2/>} />
-        <Route path="lazy3" element={<LazyPage3/>} />
-        <Route path="/*" element={<Navigate to="/lazy1" />} />
+       {definedRoutes}
+        <Route path="/*" element={<Navigate to={routes[0].to} replace/>} />
     </Routes>
    
     </div>
     </BrowserRouter>
+    </Suspense>
   )
 }
